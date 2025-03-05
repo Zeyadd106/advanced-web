7692cbaf4678d103c1fada8f61460490a80edc9c
Zeyadd106 <ziad230102788@sut.edu.eg>
2025-03-05 21:26:44 +0200

    Add files via upload
    ziad mohamed seliem // 230102788

---
+++ Assignment1/index.php
@@ -0,0 +1,62 @@
+<?php
+
+
+// EX1
+
+
+// $x = 10;
+// $y = 7;
+
+// print "$x + $y = " . ($x + $y) . "<br>";
+// print "$x - $y = " . ($x - $y) . "<br>";
+// print "$x * $y = " . ($x * $y) . "<br>";
+// print "$x / $y = " . ($x / $y) . "<br>";
+// print "$x % $y = " . ($x % $y) . "<br>";
+
+
+// EX2
+
+// $currentmonth = date('f', time());
+
+// if ($currentmonth == "August"){
+//     echo "It's August, so it's really hot.";
+// }else{
+//     echo "Not August, so at least not in the peak of the heat.";
+// }
+
+
+
+// EX3
+
+// for ($i =1; $i <=12; $i++){
+
+//     $square = $i * $i;
+
+//     echo "$i * $i = $square<br>";
+// }
+
+
+// EX4
+
+
+// echo "<table border = '1' cellspacing = '2' cellpadding = '10'>";
+
+
+// for ($i =1; $i<=7; $i++){
+//     echo "<tr> ";
+
+
+// for ($z = 1; $z <=7; $z++){
+//     echo "<td>". ($i * $z). "</td>";
+
+// }
+// echo "</tr>";
+// }
+
+// echo "</table>";
+
+
+
+
+
+?>
